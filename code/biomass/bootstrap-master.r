f5a11bf0b354a25d1e51a1ce25bc76e1d137c9cf
Wesley Brooks <brooks.wesley@gmail.com>
2013-11-08 12:39:26 -0600

    using tweedie package as installed

--- code/biomass/bootstrap-master.r
+++ code/biomass/bootstrap-master.r
@@ -2,7 +2,7 @@ library(RCurl)
 library(devtools)
 
 #These functions import code and data directly from the github servers:
-source("../brooks/code/load_https.r")
+source("../brooks/R/load_https.r")
 
 #Import the data and the heatmap code.
 source_url("https://raw.github.com/wesesque/paleon/master/code/biomass-import.r")
